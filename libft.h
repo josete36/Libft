08bb3d9ec6f3b5d58cb1ac2457e7409028d38684
josete36 <77847086+josete36@users.noreply.github.com>
2022-05-01 19:41:02 +0200

    Add files via upload

---
+++ libft.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jose <jose@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/22 18:00:10 by jomoreno          #+#    #+#             */
+/*   Updated: 2022/04/27 23:53:07 by jose             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *str);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+int	    *ft_strncmp(const char *str1, const char *str2, size_t n);
+
+#endif
---
+++ main.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jose <jose@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/22 18:49:51 by jomoreno          #+#    #+#             */
+/*   Updated: 2022/04/28 00:06:47 by jose             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+
+int	main(void)
+{
+	char		c;
+	const char	*str;
+	char		*ret;
+	const char	ch = '%';
+	const char *str1;
+	const char *str2;
+	size_t		n;
+
+	c = 't';
+	str = "caracal";
+	str1 = "Hola";
+	str2 = "Hol";
+	n = 4;
+	printf("%d \n", ft_isalpha(c));
+	printf("%d \n", ft_isdigit(c));
+	printf("%d \n", ft_isalnum(c));
+	printf("%d \n", isascii(c));
+	printf("%d \n", ft_isascii(c));
+	printf("%d \n", ft_isprint(c));
+	printf("%zu \n", ft_strlen(str));
+	printf("%zu \n", strlen(str));
+	printf("%c \n", ft_tolower(c));
+	printf("%c \n", ft_toupper(c));
+	ret = strchr(str, ch);
+	printf("string after %c is - %s\n", ch, ret);
+	printf("%s \n", ft_strchr(str, ch));
+	printf("%s \n", strrchr(str, ch));
+	printf("%s \n", ft_strrchr(str, ch));
+	printf("%d \n", strncmp(str1, str2, n));
+	
+	return (0);
+}
