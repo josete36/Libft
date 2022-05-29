7f98c449cd6e7f14c2b7581cecefb480e331c798
Jose Antonio Moreno <macjose@MBP-de-Jose.technicolor.net>
2022-05-29 13:39:39 +0200

    modified substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: macjose <macjose@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/29 11:17:17 by macjose           #+#    #+#             */
-/*   Updated: 2022/05/29 13:33:44 by macjose          ###   ########.fr       */
+/*   Updated: 2022/05/29 13:38:48 by macjose          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
 	char	*s2;
+	size_t	s_len;
 
 	s2 = malloc((len + 1) * sizeof(char));
 	if (!s2)
 		return (NULL);
+	s_len = ft_strlen(s2);
+	if (start > s_len)
+		
 	i = 0;
 	while (s[start] != '\0' && i <= len)
 	{
