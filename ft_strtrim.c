30d5fd603aeba0b8998e0a76e95dd65e88852f78
Jose antonio Moreno melgar <jomoreno@c1r1s3.42malaga.com>
2022-05-31 17:00:23 +0200

    strim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,21 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isascii.c                                       :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jomoreno <jomoreno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/31 16:36:10 by jomoreno          #+#    #+#             */
-/*   Updated: 2022/05/31 16:36:15 by jomoreno         ###   ########.fr       */
+/*   Created: 2022/05/31 16:37:46 by jomoreno          #+#    #+#             */
+/*   Updated: 2022/05/31 16:59:29 by jomoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isascii(int c)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	if (c >= 0 && c <= 127)
-		return (1);
-	else
-		return (0);
+	int	i;
+	
+	s1_len = ft_strlen(s1);
+	i = 0;
+	if (!s1)
+		return (NULL);
+	while (i < s1_len && ft_strchr(set, s1[i])
+	i++;
+	
 }
